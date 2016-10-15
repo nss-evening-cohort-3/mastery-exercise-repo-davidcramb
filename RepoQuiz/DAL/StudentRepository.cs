086ace97226888213803ad67da07750bcd8d0236
David Cramb <david.cramb@gmail.com>
2016-10-15 10:09:25 -0500

    Adding studentrepo file

---
+++ RepoQuiz/DAL/StudentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepoQuiz.DAL
+{
+    public class StudentRepository
+    {
+        public StudentContext Context { get; set; }
+        public StudentRepository()
+        {
+            Context = new StudentContext();
+        }
+        public StudentRepository(StudentContext _context)
+        {
+            Context = _context;
+        }
+
+    }
+}
