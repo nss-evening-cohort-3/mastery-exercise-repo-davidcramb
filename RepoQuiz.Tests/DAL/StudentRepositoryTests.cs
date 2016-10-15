6c1a4b3856e570fd2223b2fae7806ab8e0978fcc
David Cramb <david.cramb@gmail.com>
2016-10-15 10:12:39 -0500

    fixed build error

--- RepoQuiz.Tests/DAL/StudentRepositoryTests.cs
+++ RepoQuiz.Tests/DAL/StudentRepositoryTests.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using System.Collections.Generic;
+using RepoQuiz.DAL;
 namespace RepoQuiz.Tests.DAL
 {
     [TestClass]
     public class StudentRepositoryTests
     {
+        [TestMethod]
+        public void EnsureStudentRepoInstanceOfType()
+        {
+            StudentRepository repo = new StudentRepository();
+            Assert.IsNotNull(repo);
+        }
     }
 }
