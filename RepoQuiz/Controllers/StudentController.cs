11f7e476be56dc6d7bcfebc37fb5674b7a327073
David Cramb <david.cramb@gmail.com>
2016-10-15 13:43:13 -0500

    Table now displays with student data, pulled from database. Remaining major task: centering and make pretty

--- RepoQuiz/Controllers/StudentController.cs
+++ RepoQuiz/Controllers/StudentController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RepoQuiz.DAL;
+using RepoQuiz.Models;
 
 namespace RepoQuiz.Controllers
 {
     public class StudentController : Controller
     {
+        StudentRepository repo = new StudentRepository();
         // GET: Student
         public ActionResult Index()
         {
+            ViewBag.AllStudents = repo.GetAllStudents();
             return View();
         }
 
